8ae49923803814b8e7d24a09c81dd8f896690613
Jarosław Michna <jaroslaw.michna@gmail.com>
2017-09-05 21:10:48 +0200

    Layout mods

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,64 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-          'css/style.css': 'sass/style.sass'
-        }
-      }
+	// Project configuration.
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'css/style.css': 'sass/style.sass'
+				}
+			}
+		},
+		watch: {
+			sass: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			},
+			js: {
+				files: ['js/*.js'],
+				tasks: ['jshint'],
+				options: {
+					spawn: false,
+				},
+			}
     },
-    watch: {
-        sass: {
-            files: ['sass/*.sass'],
-            tasks: ['sass'],
-            options: {
-                spawn: false,
-            },
-        },
-        js: {
-            files: ['js/*.js'],
-            tasks: ['jshint'],
-            options: {
-                spawn: false,
-            },
-        }
-    },
-    jshint: {
-        all: ['js/*.js']
-    },
-    browserSync: {
-        dev: {
-            bsFiles: {
-                src : [
-                    'css/*.css',
-                    'js/*.js',
-                    '*.html'
-                ]
-            },
-            options: {
-                watchTask: true,
-                server: './'
-            }
-        }
-    }
-  });
+		jshint: {
+			options: {
+				eqeqeq:true,
+				esversion: 6
+			},	
+			all: ['js/*.js']
+		},
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src : [
+						'css/*.css',
+						'js/*.js',
+						'*.html'
+					]
+				},
+				options: {
+					watchTask: true,
+					server: './'
+				}
+			}
+		}
+	});
 
-  //Load the plugins tasks}
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-browser-sync');
+	//Load the plugins tasks}
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-browser-sync');
 
-  // Default tasks
-  grunt.registerTask('default', ['sass', 'jshint', 'browserSync', 'watch']);
+	// Default tasks
+	grunt.registerTask('default', ['sass', 'jshint', 'browserSync', 'watch']);
 };
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # RockPaperScissors
 <h2>Rock, Papers, Scissors - The Game</h2>
-<p>Used: Boostrap, FontAwesome, Sass, JavaScript</p>
+<p>Date: August 2017 r.</p>
+<p><strong>Used</strong>: Boostrap, FontAwesome, Sass, JavaScript.</p>
+<p><strong>Working with</strong>: Visual Studio Code, Grunt, Jshint.</p>
